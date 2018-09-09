a7c566156b2252eb912d485b5621c5ac1ddba549
Amirreza Amouie <aamouie01@gmail.com>
2018-09-10 00:19:18 +0430

    check whether the users list can be updated

--- project/DataCollector/main.py
+++ project/DataCollector/main.py
@@ -1,10 +1,9 @@
 from DataCollector import gather, scan_users_list, clean, save
 
 # This line of code isn't always executed:
-# scanner = scan_users_list.ScanUsers().scan()
+scanner = scan_users_list.ScanUsers().scan()
 
-# statuses_list = gather.Gatherer().gather()
-all_statuses = gather.Gatherer().gather_all()
-cleaned_statuses_list = clean.clean(all_statuses)
-save.write_json(cleaned_statuses_list)
+# all_statuses = gather.Gatherer().gather_all()
+# cleaned_statuses_list = clean.clean(all_statuses)
+# save.write_json(cleaned_statuses_list)
 
--- project/DataCollector/scan_users_list.py
+++ project/DataCollector/scan_users_list.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 from DataCollector import connect
@@ -5,13 +6,44 @@ from DataCollector import connect
 
 class ScanUsers:
     api = connect.Connector().create_client()
+    last_time = '9-8-2018'
 
     def scan(self):
-        # get the list of users
-        users = self.api.GetFriends(screen_name="BestFarsi")
+        if self.can_scan_users():
+            # get the list of users
+            users = self.api.GetFriends(screen_name="BestFarsi")
+            # write to the userslist.txt file
+            date = datetime.datetime.now().strftime('%m-%d-%Y')
+            f = open("../Data/users/userslist-%s.txt" % date, "w")
+            for user in users:
+                f.write(user.screen_name + "\n")
+                print(user.screen_name)
+        else:
+            pass
 
-        # write to the userslist.txt file
-        f = open("static_files/userslist.txt", "a")
-        for user in users:
-            f.write(user.screen_name + "\n")
-            print(user.screen_name)
+    def can_scan_users(self):
+        current_time = datetime.datetime.now().strftime('%m-%d-%Y')
+        return self.update_users_scan_time(current_time)
+
+    def update_users_scan_time(self, current_time):
+        last_time_cleaned = self.last_time.split('-')
+        current_time_cleaned = current_time.split('-')
+        # year
+        if last_time_cleaned[2] == current_time_cleaned[2]:
+            # month
+            if last_time_cleaned[0] == current_time_cleaned[0]:
+                # day
+                if int(last_time_cleaned[1]) < int(current_time_cleaned[1]):
+                    # I'm not sure whether i should compare equals or compare if one is bigger than the other.
+                    self.last_time = current_time
+                    print("Can update.")
+                    return True
+            else:
+                self.last_time = current_time
+                print("Can update.")
+                return True
+        else:
+            self.last_time = current_time
+            print("Can update.")
+            return True
+        return False
