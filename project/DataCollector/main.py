6ef1d7fee96a40aef03e62e970fc44ed9fd76be7
Amirreza Amouie <aamouie01@gmail.com>
2018-09-11 11:40:21 +0430

    keep track of users list's updates and write the code that manages daily scans

--- project/DataCollector/main.py
+++ project/DataCollector/main.py
@@ -1,9 +1,9 @@
 from DataCollector import gather, scan_users_list, clean, save
 
 # This line of code isn't always executed:
-# scanner = scan_users_list.ScanUsers().scan()
+scanner = scan_users_list.ScanUsers().scan()
 
-all_statuses = gather.Gatherer().gather_all()
-cleaned_statuses_list = clean.clean(all_statuses)
-save.write_json(cleaned_statuses_list)
+# all_statuses = gather.Gatherer().gather_all()
+# cleaned_statuses_list = clean.clean(all_statuses)
+# save.write_json(cleaned_statuses_list)
 
