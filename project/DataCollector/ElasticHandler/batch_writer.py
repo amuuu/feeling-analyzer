d647612369116ba330867862480d24848532e0c0
Amirreza Amouie <aamouie01@gmail.com>
2018-09-26 16:19:17 +0330

    change the batch writing's initial index

--- project/DataCollector/ElasticHandler/batch_writer.py
+++ project/DataCollector/ElasticHandler/batch_writer.py
@@ -4,6 +4,6 @@ from DataCollector.Preprocessor import clean
 
 def write_batch(tweet_dict):
     cleaned_statuses_list = clean.clean(tweet_dict)
-    print("SIZE: ", len(tweet_dict))
+    # print("Writing", len(tweet_dict))
     last_batch_index = elastic_tweets.get_last_tweet_id()
     elastic_tweets.write_tweets(cleaned_statuses_list, last_batch_index)
--- project/DataCollector/ElasticHandler/elastic_tweets.py
+++ project/DataCollector/ElasticHandler/elastic_tweets.py
@@ -4,9 +4,8 @@ es = Elasticsearch()
 
 
 def write_tweets(tweets, start_index):
-    index = 0
+    index = 1
     for tweet in tweets:
-
         res = es.index(index="tweets_index", doc_type='tweets_doc', id=start_index+index, body=tweets.get(str(index)))
         index += 1
         print(res['result'])
