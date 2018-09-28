3b769db89475eade6ad94318015b747ce86dca33
Amirreza Amouie <aamouie01@gmail.com>
2018-09-28 23:50:19 +0330

    add error index initializer

--- project/DataCollector/ElasticHandler/elastic_errors.py
+++ project/DataCollector/ElasticHandler/elastic_errors.py
@@ -5,10 +5,19 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch()
 
 
+def init_error_index():
+    return es.index(index="errors_index", doc_type='errors_doc', id=0, body={})
+
+
+
 def write_error(error):
+    print("LAST  INDEX OF ERROR:", get_last_error_index())
     res = es.index(index="errors_index", doc_type='errors_doc', id=get_last_error_index(), body=error)
     print('error logged ', res['result'])
 
 
 def get_last_error_index():
-    return es.count(index='errors_index', doc_type='errors_doc').get('count')
+    return int(es.count(index='errors_index', doc_type='errors_doc').get('count'))
+
+
+init_error_index()
